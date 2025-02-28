750635422bcd31bd6d79235be373be1eabedff32
nda <nda994@gmail.com>
2025-02-28 05:31:16 +0100

    add MulVAL

---
+++ MulVAL/architecture.pl
@@ -0,0 +1,10 @@
+/* Attack goal */
+attackGoal(canSpoof(feedbackFlow4)).
+
+/* Attacker location */
+attackerLocated(coverZone).
+
+/* Control flow satellite to GPS Module */
+controlFlow(satellite, gpsModule, feedbackFlow4).
+physicalLayer(nmea0183, gpsModule, satellite).
+weaknessPhysicalLayer(ac7, nmea0183, coverZone).
